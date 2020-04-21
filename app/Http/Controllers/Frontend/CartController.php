5be02d06bb23951d7f6d73c7005a44fb726156fd
Till Schander <tillschander@gmx.de>
2020-04-21 18:04:16 +0200

    AddToCart-Methode

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -14,7 +14,32 @@ class CartController extends Controller
 
     public function addToCart(Request $request)
     {
-        dd($request->all());
+        // validate input
+        $data = $request->validate([
+            'id' => 'required|integer',
+            'qty' => 'required|integer'
+        ]);
+
+        // get cart from session
+        $cart = [];
+        if (session()->has('cart')) {
+            $cart = session()->get('cart');
+        }
+
+        // add product to cart
+        $id = (int) $data['id'];
+        $qty = (int) $data['qty'];
+        if (isset($cart[$id])) {
+            $cart[$id] += $qty;
+        } else {
+            $cart[$id] = $qty;
+        }
+
+        // put cart into session
+        session()->put('cart', $cart);
+        
+        // redirect to cart view
+        return redirect('/cart');
     }
 
     public function updateCart()
