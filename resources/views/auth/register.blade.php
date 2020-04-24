945c4677d78b849fa9cce6bf5385b154187cd589
Till Schander <tillschander@gmx.de>
2020-04-24 13:10:07 +0200

    Auth styling

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,17 @@
-@extends('layouts.app')
+@extends('auth/layouts.app')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">
+                    <h2 class="pull-left m-0">{{ __('Register') }}</h2>
+
+                    <a class="pull-right mt-2" href="{{ route('login') }}">
+                        {{ __('Login') }}
+                    </a>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
