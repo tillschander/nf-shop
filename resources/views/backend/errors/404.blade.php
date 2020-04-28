09ebab2277447bbf4d70623944bf41c58cb8fa3a
Till Schander <tillschander@gmx.de>
2020-04-28 12:57:11 +0200

    custom 404 pages

---
+++ resources/views/backend/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('backend/layouts/app')
+
+@section('content')
+<div class="container">
+    <div class="row align-items-center">
+        <h1>404 - no found</h1>
+    </div>
+</div>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('frontend/layouts/app')
+
+@section('content')
+<div class="container">
+    <div class="row align-items-center">
+        <h1 class="mt-5">404 - no found</h1>
+    </div>
+</div>
+@endsection
